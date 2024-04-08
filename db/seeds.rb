25770e9c9831dbafbff6b3da8f344df1f9e1247b
laura vidal <lauravidal.vl@gmail.com>
2024-04-08 16:31:42 +0200

    Add dotenv-rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,43 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'open-uri'
+require 'json'
+
+puts "Cleaning up database..."
+Movie.destroy_all
+puts "Database cleaned"
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies = JSON.parse(URI.open(url).read)["results"]
+
+# for i in 0..9 do
+#   movies = JSON.parse(URI.open(url).read)["results"]
+#   puts "Creating #{movies[i]["title"]}"
+#   base_poster_url = "https://image.tmdb.org/t/p/original"
+#   Movie.create(
+#     title: movies[i]["title"],
+#     overview: movies[i]["overview"],
+#     poster_url: "#{base_poster_url}#{movies[i]["backdrop_path"]}",
+#     rating: movies[i]["vote_average"]
+#   )
+# end
+# puts "Movies created"
+
+# 10.times do |i|
+#   movies = JSON.parse(URI.open(url).read)["results"][i]
+#   base_poster_url = "https://image.tmdb.org/t/p/original"
+#   Movie.create(
+#     title: movies["title"],
+#     overview: movies["overview"],
+#     poster_url: "#{base_poster_url}#{movies["backdrop_path"]}",
+#     rating: movies["vote_average"]
+#   )
+# end
+# puts "Movies created"
+
+List.create!(name: "Top 10 Movies")
+List.create!(name: "Classic movies")
+List.create!(name: "Superhero movies")
+
+puts "List created"
